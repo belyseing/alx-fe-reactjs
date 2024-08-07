ca1c4bf1c682f721603a413327e13e1160f40a9f
Belyse Ingabire <ingabelyse53gmail.com>
2024-08-07 21:21:19 +0200

    update: Created Specific Components in a React Application

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,7 +1,11 @@
-import { useState } from 'react'
+import { useState } from 'react';
 import reactLogo from './assets/react.svg';
 import viteLogo from '/vite.svg';
 import './App.css';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+
 import WelcomeMessage from './components/WelcomeMessage';
 
 
@@ -9,6 +13,8 @@ function App() {
   const [count, setCount] = useState(0);
   return (
     <>
+    <Header />
+      <MainContent/>
       <WelcomeMessage /> {/* Include the WelcomeMessage component here */}
 
       <div>
@@ -31,6 +37,7 @@ function App() {
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
+      <Footer />
     </>
   );
 }
