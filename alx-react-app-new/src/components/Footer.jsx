278b42b8349e74898be7182509406bd472867d5d
Belyse Ingabire <ingabelyse53gmail.com>
2024-08-12 21:04:54 +0200

    complete:Apply Inline CSS Styling to React Components

--- alx-react-app-new/src/components/Footer.jsx
+++ alx-react-app-new/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 function Footer(){
   return(
-    <footer>
+    <footer style={{backgroundcolor: '#f0f0f0', padding: '20px', fontFamily : 'Arial, sans-serif', color : '#333', borderRadius: '10px'}}>
   <p>© 2023 City Lovers</p>
 </footer>
   );
--- alx-react-app-new/src/components/MainContent.jsx
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -1,8 +1,8 @@
 import React from 'react';
  
-function MainContent(){
+function MainContent(){  
   return(
-    <main>
+    <main style={{backgroundcolor: '#f0f0f0', padding: '20px', fontFamily : 'Arial, sans-serif', color : '#333', borderRadius: '10px'}}>
   <p>I love to visit New York, Paris, and Tokyo.</p>
 </main>
   );
