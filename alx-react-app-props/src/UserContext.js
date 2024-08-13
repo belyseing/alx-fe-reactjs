7d33a37a92d987b6375f91aa2f8b6f53c697521e
Belyse Ingabire <ingabelyse53gmail.com>
2024-08-13 22:56:29 +0200

    update:Refactor Prop Drilling to Use Context API

--- alx-react-app-props/src/UserContext.js
+++ alx-react-app-props/src/UserContext.js
@@ -1,5 +1,7 @@
 import { createContext } from "react";
 
+// Initialize a new context called UserContext
 const UserContext = createContext();
 
+// Export the UserContext so it can be used in other parts of the application
 export default UserContext;
