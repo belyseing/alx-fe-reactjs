d71afaef8c4ecb905bba2281ae17acbb51a3f1dd
Belyse Ingabire <ingabelyse53gmail.com>
2024-08-17 18:20:35 +0200

    Build a Simple Company Website with React

---
+++ my-company/src/App.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Service from "./pages/Services";
+
+const App = () => {
+  return (
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Services />} />
+      </Routes>
+    </Router>
+  );
+};
+
+export default App;
