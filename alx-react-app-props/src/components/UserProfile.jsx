8aaa9bd6b7df71e4669b1f1b76a5159d355abaf4
Belyse Ingabire <ingabelyse53gmail.com>
2024-08-17 16:47:29 +0200

    update userprofile

--- alx-react-app-props/src/components/UserProfile.jsx
+++ alx-react-app-props/src/components/UserProfile.jsx
@@ -1,14 +1,15 @@
-import React from 'react';
-
+import { useContext } from "react";
+import UserContext from "./UserContext";
 
 const UserProfile = (props) => {
-   return (
-     <div>
-       <h2>{props.name}</h2>
-       <p>Age: {props.age}</p>
-       <p>Bio: {props.bio}</p>
-     </div>
-   );
- };
+  const userData = useContext(UserContext);
+  return (
+    <div>
+      <h2>{userData.name}</h2>
+      <p>Age: {props.age}</p>
+      <p>Bio: {props.bio}</p>
+    </div>
+  );
+};
 
- export default UserProfile;
+export default UserProfile;
