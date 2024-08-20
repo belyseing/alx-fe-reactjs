d0a0c1861d9f36707cd704b25b58fe1e845a63ec
Belyse Ingabire <ingabelyse53gmail.com>
2024-08-20 20:12:29 +0200

    Application with Zustand State Management

---
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -0,0 +1,18 @@
+import React from "react";
+import RecipeList from "./components/RecipeList";
+import AddRecipeForm from "./components/AddRecipeForm";
+
+
+const App = () => {
+  return (
+    <div>
+      <h1>Recipe Manager</h1>
+      {/* Display the list of recipes */}
+      <RecipeList />
+      {/* Form to add new recipes */}
+      <AddRecipeForm />
+    </div>
+  );
+};
+
+export default App;
