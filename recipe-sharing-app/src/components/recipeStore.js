036869a7282539915f1453fd09711866dd6c788d
Belyse Ingabire <ingabelyse53gmail.com>
2024-08-20 20:30:05 +0200

    Application with Zustand State Management

--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -1,18 +1,10 @@
-import React from "react";
-import RecipeList from "./components/RecipeList";
-import AddRecipeForm from "./components/AddRecipeForm";
+import create from "zustand";
 
+const useRecipeStore = create((set) => ({
+  recipes: [],
+  addRecipe: (newRecipe) =>
+    set((state) => ({ recipes: [...state.recipes, newRecipe] })),
+  setRecipes: (recipes) => set({ recipes }),
+}));
 
-const App = () => {
-  return (
-    <div>
-      <h1>Recipe Manager</h1>
-      {/* Display the list of recipes */}
-      <RecipeList />
-      {/* Form to add new recipes */}
-      <AddRecipeForm />
-    </div>
-  );
-};
-
-export default App;
+export default useRecipeStore;
