176f3206f14bbf3f20dc07b03dda8c6fda9eac08
Belyse Ingabire <ingabelyse53gmail.com>
2024-08-21 15:17:42 +0200

    Application with Detailed Recipe Management

--- recipe-sharing-app/src/App.jsx
+++ recipe-sharing-app/src/App.jsx
@@ -1,15 +1,20 @@
 import React from "react";
 import RecipeList from "./components/RecipeList";
 import AddRecipeForm from "./components/AddRecipeForm";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import RecipeDetails from "./components/RecipeDetails";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 // Import other components as needed
 
 const App = () => {
   return (
     <Router>
       <Routes>
-        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />
+        <Route path="/" element={<RecipeList />} />{" "}
+        {/* Route for the homepage */}
+        <Route path="/recipe/:recipeId" element={<RecipeDetails />} />{" "}
+        {/* Route for recipe details */}
+        <Route path="/add-recipe" element={<AddRecipeForm />} />{" "}
+        {/* Route for adding a new recipe */}
         {/* Add other routes here */}
       </Routes>
     </Router>
