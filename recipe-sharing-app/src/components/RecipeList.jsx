a58988bd52665b65bf02fd06c025ed9fca04860c
Belyse Ingabire <ingabelyse53gmail.com>
2024-08-22 22:38:40 +0200

    Application with Detailed Recipe Management

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -1,18 +1,24 @@
-// RecipeList component
-import useRecipeStore from "./recipeStore";
+import { useRecipeStore } from "./recipeStore";
 
 const RecipeList = () => {
   const recipes = useRecipeStore((state) => state.recipes);
-  console.log(recipes);
+
+  if (recipes.length === 0) {
+    return <p>No recipes found. Add a new recipe!</p>;
+  }
+
   return (
     <div>
-      {" "}  
-      {recipes.map((recipe) => (
-        <div key={recipe.id}>
-          <h3>{recipe.title}</h3>
-          <p>{recipe.description}</p>
-        </div>
-      ))}
+      <h2>Recipe List</h2>
+      <ul>
+        {recipes.map((recipe) => (
+          <li key={recipe.id}>
+            <h3>{recipe.title}</h3>
+            <p>{recipe.description}</p>
+            {/* Add buttons for editing and deleting if needed */}
+          </li>
+        ))}
+      </ul>
     </div>
   );
 };
