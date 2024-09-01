371dc5b3554f0ba1dc9ace36f287202cc4d83279
Belyse Ingabire <ingabelyse53gmail.com>
2024-09-01 19:06:38 +0200

    Implementing Form Handling in React with Controlled Components and Formik

--- form-handling-react/src/components/formikForm.js
+++ form-handling-react/src/components/formikForm.js
@@ -17,7 +17,7 @@ const FormikForm = () => {
     >
       <Form>
         <div>
-          <label htmlFor="username"></label>
+          <label htmlFor="username">Username</label>
           <Field name="username" type="text" />
           <ErrorMessage name="username" component="div" />
         </div>
@@ -41,4 +41,3 @@ const FormikForm = () => {
 };
 
 export default FormikForm;
-
