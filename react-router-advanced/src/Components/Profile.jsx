80b8384eaf2735b7a91ca1573374ef445241dcc1
Belyse Ingabire <ingabelyse53gmail.com>
2024-09-01 21:26:30 +0200

    Advanced Routing in React with React Router

--- react-router-advanced/src/Components/Profile.jsx
+++ react-router-advanced/src/Components/Profile.jsx
@@ -4,10 +4,7 @@ function Profile() {
   return (
     <div>
       <h2>Profile Page</h2>
-      <Routes>
-        <Route path="details" element={<ProfileDetails />} />
-        <Route path="settings" element={<ProfileSettings />} />
-      </Routes>
+      <p>This is the profile page.</p>
     </div>
   );
 }
