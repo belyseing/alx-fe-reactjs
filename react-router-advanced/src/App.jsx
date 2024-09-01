bda15c91e6335501ba44a71af6e94215cedb6bff
Belyse Ingabire <ingabelyse53gmail.com>
2024-09-01 21:16:44 +0200

    Advanced Routing in React with React Router

--- react-router-advanced/src/App.jsx
+++ react-router-advanced/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Post from "./components/Post";
 import Home from "./Components/Home";
+import About from "./Components/About";
 import Profile from "./components/Profile";
 import ProtectedRoute from "./components/ProtectedRoute";
 
