c16364220c3d5d4f8a0738e24b3f9af92f9a6ca4
Belyse Ingabire <ingabelyse53gmail.com>
2024-09-11 10:08:21 +0200

    React Application for a Recipe Sharing Platform with Tailwind CSS

---
+++ recipe-sharing-platform/src/App.jsx
@@ -0,0 +1,11 @@
+import { useState } from "react";
+
+function App() {
+  return (
+    <div className="text-blue-500">
+      <h1>Hello, Recipe Sharing Platform!</h1>
+    </div>
+  );
+}
+
+export default App;
