8b9f4b4d2451d8609f54de6ef660c46330edc067
belyseing <ingabelyse53@gmail.com>
2024-09-21 21:31:04 +0100

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,11 +1,28 @@
 import axios from 'axios';
 
-const fetchUserData = async (username) => {
+const fetchUserData = async (username, location = '', minRepos = 0) => {
     if (!username) throw new Error('Username is required.');
+
+
+    // Construct the query string
+    let query = `q=${username}`;
+    
+    // Add location to the query if provided
+    if (location) {
+        query += `+location:${location}`;
+    }
+
+    // Add minimum repositories to the query if provided
+    if (minRepos > 0) {
+        query += `+repos:>${minRepos}`;
+    }
+
+    // Construct the API endpoint
+    const url = `https://api.github.com/search/users?${query}`;
 
     try {
         const response = await axios.get(`https://api.github.com/users/${username}`);
-        return response.data;
+        return response.data.items; // Return the items (array of users)
     } catch (error) {
         if (error.response && error.response.status === 404) {
             throw new Error("Looks like we cant find the user");
